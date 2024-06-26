8ac8566c596fbfb06251d24843c9d475dd179549
sagiri <guowenjie1970@163.com>
2024-06-26 23:18:24 +0800

    学完c++哩

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,44 +1,37 @@
-#include "iostream"
-#include "vector"
-#include "string"
+# include "iostream"
 using namespace std;
-int forceSearch(int arr[],int length,int x){
-    for (int i = 0; i < length; ++i) {
-        if (arr[i]==x){
-            return i;
+#include "SqList.h"
+
+void changeMaxMin(SqList &sq){
+    ElemType maxValue,minValue;
+    int maxLocation,minLocation;
+    for(int location=1;location<= GetLenth(sq);location++){
+        ElemType value;
+        GetElem(sq,location,&value);
+        if(location==1){
+            maxValue=value;
+            minValue=value;
+        }
+        if(value>maxValue){
+            maxValue=value;
+            maxLocation=location;
+        }
+        if(value<minValue){
+            minValue=value;
+            minLocation=location;
         }
     }
-    return -1;
-}
-int binarySearch(int arr[],int length,int x){
-    int left=0,right=length-1;
-    while (left<=right){
-        int mid=(right-left)/2+left;
-        if(arr[mid]==x){
-            return mid;
-        }
-        else if(x>arr[mid]){
-            left=mid+1;
-        }
-        else if(x<arr[mid]){
-            right=mid-1;
-        }
-    }
-    return -1;
+    ListDelete(&sq,maxLocation);
+    ListInsert(&sq,maxLocation,minValue);
+    ListDelete(&sq,minLocation);
+    ListInsert(&sq,minLocation,maxValue);
 }
 int main(){
-    int n=0;int sum=0;
-    cout<<"enter n: ";
-    cin>>n;
-    cout<<"\n输入n个值: ";
-    vector<int> vector1;
-    for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        vector1.push_back(x);
-    }
-for(int i:vector1){
-    sum=sum+i;
-}
-cout<<n<<"个值的平均值为:"<<(float )sum/(float )n;
+    SqList sq;
+    InitList(sq);
+    ElemType arr[5]={3,2,9,4,7};
+    CreateList(sq,arr,5);
+    DispList(sq);
+    changeMaxMin(sq);
+    DispList(sq);
 }
